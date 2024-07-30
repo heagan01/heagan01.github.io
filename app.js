f8ac6f7014a1d012e4c3693c145b08b41fd8c468
heagan01 <54394590+heagan01@users.noreply.github.com>
2024-07-30 16:34:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ let material = {
             "url": "https://docs.google.com/presentation/d/1cvpJ4nQRLtQAmBGCktK11NhsrES4wgaQ/edit?usp=drive_link&ouid=101549332984118855678&rtpof=true&sd=true"
         },
         {
-            "class": "a",
+            "class": "a (for testing)",
             "title": "Present Perfect and Past Simple",
             "url": "https://docs.google.com/presentation/d/1cvpJ4nQRLtQAmBGCktK11NhsrES4wgaQ/edit?usp=drive_link&ouid=101549332984118855678&rtpof=true&sd=true"
         },
         {
-            "class": "a",
+            "class": "a (for testing)",
             "title": "Present Perfect and Past Simple",
             "url": "https://docs.google.com/presentation/d/1cvpJ4nQRLtQAmBGCktK11NhsrES4wgaQ/edit?usp=drive_link&ouid=101549332984118855678&rtpof=true&sd=true"
         },
         {
-            "class": "a",
+            "class": "a (for testing)",
             "title": "Present Perfect and Past Simple",
             "url": "https://docs.google.com/presentation/d/1cvpJ4nQRLtQAmBGCktK11NhsrES4wgaQ/edit?usp=drive_link&ouid=101549332984118855678&rtpof=true&sd=true"
         },
         {
-            "class": "a",
+            "class": "a (for testing)",
             "title": "Present Perfect and Past Simple",
             "url": "https://docs.google.com/presentation/d/1cvpJ4nQRLtQAmBGCktK11NhsrES4wgaQ/edit?usp=drive_link&ouid=101549332984118855678&rtpof=true&sd=true"
         },
@@ -39,35 +39,50 @@ let material = {
             "class": "Matematika",
             "title": "Bilangan Berpangkat 3",
             "url": "https://drive.google.com/file/d/1vtupJnskcsALt7d3wjmRfYleWLncGYIn/view?usp=drive_link"
+        },
+        {
+            "class": "Matematika",
+            "title": "Bilangan Berpangkat 4",
+            "url": "https://drive.google.com/file/d/1DRC2rgZrGjT1SbZdlO6OelZEN-MhqMXp/view?usp=drive_link"
         }
     ]
 }
+
+
 let list_of_subjects = []
+let list_of_subjects_raw = []
 
 material['materials'].forEach(element => {
-    list_of_subjects.push(element['class'])
+    list_of_subjects_raw.push(element['class'])
 })
 
 function onlyUnique(value, index, array) {
     return array.indexOf(value) === index;
 }
 
-list_of_subjects = list_of_subjects.filter(onlyUnique);
+list_of_subjects = list_of_subjects_raw.filter(onlyUnique);
 
 const column_parent = document.getElementsByClassName('columns')
 
-function generateColumns(data) {
+function generateColumns() {
     // console.log(list_of_subjects.length)
     // console.log(list_of_subjects)
     
-    data.forEach(element => {
+    list_of_subjects.forEach(element => {
         let column = document.createElement('div')
-        column.setAttribute('class', 'column is-narrow')
+        column.setAttribute('class', 'column')
         // const column_child = document.createElement('a')
         // column_child.setAttribute('class', 'notification is-link')
         const column_child = document.createElement('a')
-        column_child.setAttribute('class', 'button')
-        column_child.innerHTML = element
+        column_child.setAttribute('class', 'box')
+        let count = 0
+        list_of_subjects_raw.forEach( subject => {
+            if (subject == element) {
+                count += 1
+            }
+        })
+        // console.log(`${element}: ${count}`)
+        column_child.innerHTML = `${element} (${count})`
         column_child.setAttribute('href', `details.html?class=${element}`)
         column.appendChild(column_child)
         column_parent[0].appendChild(column)
@@ -84,13 +99,23 @@ function generateColumnsDetails(data) {
         // const column_child = document.createElement('a')
         // column_child.setAttribute('class', 'notification is-link')
         const column_child = document.createElement('a')
-        column_child.setAttribute('class', 'button')
+        column_child.setAttribute('class', 'box')
         
         column_child.innerHTML = element['title']
         column_child.setAttribute('href', element['url'])
         column.appendChild(column_child)
         column_parent[0].appendChild(column)
     })
+}
+
+function generateBreadcrumb(subject) {
+    const breadcrumb = document.getElementById('ul')
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    li.setAttribute('class', 'is-active')
+    a.innerHTML = subject
+    li.appendChild(a)
+    breadcrumb.appendChild(li)
 }
 
 function getDetails() {
@@ -101,5 +126,6 @@ function getDetails() {
     console.log(filtered_materials)
     generateColumnsDetails(filtered_materials)
 
-    document.getElementById('title').innerHTML = urlParams.get('class')
+    document.getElementById('title').innerHTML = `List of materials for ${urlParams.get('class')}`
+    generateBreadcrumb(urlParams.get('class'))
 }
